4e7fd6f56aa9ab18ad4712f8ceceab0ee71ab85e
Andreas Lutro <anlutro@gmail.com>
2022-03-09 11:29:31 +0100

    account for systems with python3 but no python3.x

--- psm.bash
+++ psm.bash
@@ -4,7 +4,7 @@ set -eu
 [ -z "${PSM_VENV_DIR-}" ] && PSM_VENV_DIR=~/.local/share/psm
 [ -z "${PSM_BIN_DIR-}" ] && PSM_BIN_DIR=~/.local/bin
 [ -z "${PSM_PYTHON-}" ] && PSM_PYTHON=$(
-    find $HOME/.local/bin /usr/local/bin /usr/bin -regex '.*/python[3-9]\.[0-9]+' -printf '%f\n' \
+    find $HOME/.local/bin /usr/local/bin /usr/bin -regex '.*/python[3-9][0-9.]*' -printf '%f\n' \
     | sort -V | tail -1
 )
 PSM_PYTHON_VER=$($PSM_PYTHON --version 2>&1 | cut -d' ' -f2)
